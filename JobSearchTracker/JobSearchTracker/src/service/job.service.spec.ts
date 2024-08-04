e2df994d34421867a51f62f5c989987e200de374
Daron Rice <daronrice726@gmail.com>
2024-08-04 10:11:12 -0400

    added unit test for UI

--- JobSearchTracker/JobSearchTracker/src/service/job.service.spec.ts
+++ JobSearchTracker/JobSearchTracker/src/service/job.service.spec.ts
@@ -1,16 +1,62 @@
 import { TestBed } from '@angular/core/testing';
 
 import { JobService } from './job.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { JTSJob } from 'src/model/job';
 
 describe('JobService', () => {
   let service: JobService;
+  let httpClient:HttpClient;
+  let jobID:number = 1;
+  let job:JTSJob;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
+    httpClient = TestBed.inject(HttpClient);
     service = TestBed.inject(JobService);
+    service = new JobService(httpClient);
+    job = new JTSJob();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('#getJobByID should return JTSJob or undefined', (done: DoneFn) => {
+    service?.getJobByID(jobID)?.subscribe((value) => {
+       expect (typeof(value) == typeof(Observable<JTSJob>) || typeof(value) == undefined).toBe(true);
+      done();
+    });
+  });
+
+  it('#getAllJobs should return JTSJob[] or undefined', (done: DoneFn) => {
+    service?.getAllJobs()?.subscribe((value) => {
+       expect (typeof(value) == typeof(Observable<JTSJob[]>) || typeof(value) == undefined).toBe(true);
+      done();
+    });
+  });
+
+  it('#getLastJobID should return number or undefined', (done: DoneFn) => {
+    service?.getLastJobID()?.subscribe((value) => {
+       expect (typeof(value) == typeof(Observable<Number>) || typeof(value) == undefined).toBe(true);
+      done();
+    });
+  });
+
+  it('#addJob should return JTSJob or undefined', (done: DoneFn) => {
+    service?.addJob(job)?.subscribe((value) => {
+       expect (typeof(value) == typeof(Observable<JTSJob>) || typeof(value) == undefined).toBe(true);
+      done();
+    });
+  });
+
+  it('#deleteJob should return JTSJob or undefined', (done: DoneFn) => {
+    service?.deleteJob(-1)?.subscribe((value) => {
+       expect (typeof(value) == typeof(Observable<JTSJob>) || typeof(value) == undefined).toBe(true);
+      done();
+    });
+  });
+
+
 });
--- JobSearchTracker/JobSearchTracker/src/service/notification.service.spec.ts
+++ JobSearchTracker/JobSearchTracker/src/service/notification.service.spec.ts
@@ -1,16 +1,61 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NotificationService } from './notification.service';
+import { HttpClient } from '@angular/common/http';
+import { JTSNotification } from 'src/model/notification';
+import { Observable } from 'rxjs';
 
 describe('NotificationService', () => {
   let service: NotificationService;
-
+  let httpClient:HttpClient;
+  let notificationID:number = 1;
+  let notification:JTSNotification;
+  
   beforeEach(() => {
     TestBed.configureTestingModule({});
+    httpClient = TestBed.inject(HttpClient);
     service = TestBed.inject(NotificationService);
+    service = new NotificationService(httpClient);
+    notification = new JTSNotification();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('#getNotificationByID should return JTSNotification or undefined', (done: DoneFn) => {
+    service?.getNotificationByID(notificationID)?.subscribe((value) => {
+       expect (typeof(value) == typeof(Observable<JTSNotification>) || typeof(value) == undefined).toBe(true);
+      done();
+    });
+  });
+
+  it('#getAllNotifications should return JTSNotification[] or undefined', (done: DoneFn) => {
+    service?.getAllNotifications()?.subscribe((value) => {
+       expect (typeof(value) == typeof(Observable<JTSNotification[]>) || typeof(value) == undefined).toBe(true);
+      done();
+    });
+  });
+
+  it('#getLastNotificationID should return number or undefined', (done: DoneFn) => {
+    service?.getLastNotificationID()?.subscribe((value) => {
+       expect (typeof(value) == typeof(Observable<Number>) || typeof(value) == undefined).toBe(true);
+      done();
+    });
+  });
+
+  it('#addNotification should return JTSNotification or undefined', (done: DoneFn) => {
+    service?.addNotification(notification)?.subscribe((value) => {
+       expect (typeof(value) == typeof(Observable<JTSNotification>) || typeof(value) == undefined).toBe(true);
+      done();
+    });
+  });
+
+  it('#deleteNotification should return JTSNotification or undefined', (done: DoneFn) => {
+    service?.deleteNotification(-1)?.subscribe((value) => {
+       expect (typeof(value) == typeof(Observable<JTSNotification>) || typeof(value) == undefined).toBe(true);
+      done();
+    });
+  });
+
 });
