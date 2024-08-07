abae33fe3bd05be18f6d260fc9c0a6c987ed0ea1
Daron Rice <daronrice726@gmail.com>
2024-08-07 00:11:49 -0400

    added code to deal with identity column

--- JobTrackerAPI/Context/JobTrackerAPIDataContext.cs
+++ JobTrackerAPI/Context/JobTrackerAPIDataContext.cs
@@ -3,6 +3,7 @@ using JobTrackerAPI.Model;
 using JobTrackerAPI.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using System.Xml;
 
 namespace IntegraPartnersContactApplicationAPI
 {
@@ -17,7 +18,20 @@ namespace IntegraPartnersContactApplicationAPI
 
         }
 
-       
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Job>(b =>
+            {
+                b.HasKey(e => e.JobID);
+                b.Property(e => e.JobID).ValueGeneratedOnAdd();
+            });
+            modelBuilder.Entity<Notification>(b =>
+            {
+                b.HasKey(e => e.NotificationID);
+                b.Property(e => e.NotificationID).ValueGeneratedOnAdd();
+            });
+        }
+
         public virtual DbSet<Job> Job { get; set; } = null!;
         public virtual DbSet<Notification> Notification { get; set; } = null!;
 
